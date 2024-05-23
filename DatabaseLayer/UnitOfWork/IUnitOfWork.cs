c0123b22efd67a22d2efcbdd10e8982896e756f4
Teddy55Codes <teddy@teddy.email>
2024-05-23 23:19:27 +0200

    now using repository interfaces in unit of work pattern

--- DatabaseLayer/UnitOfWork/IUnitOfWork.cs
+++ DatabaseLayer/UnitOfWork/IUnitOfWork.cs
@@ -1,14 +1,15 @@
 ﻿using DataBaseLayer.Repositories;
+using DataBaseLayer.Repositories.Interfaces;
 
 namespace DatabaseLayer.UnitOfWork;
 
 public interface IUnitOfWork : IDisposable
 { 
-    public ChatRepository Chats { get; }
-    public MessageRepository Messages { get; }
-    public PermissionRepository Permissions { get; }
-    public RoleRepository Roles { get; }
-    public UserRepository Users { get; }
+    public IChatRepository Chats { get; }
+    public IMessageRepository Messages { get; }
+    public IPermissionRepository Permissions { get; }
+    public IRoleRepository Roles { get; }
+    public IUserRepository Users { get; }
     
     public void Commit();
     public void ApplyMigrations();
--- DatabaseLayer/UnitOfWork/UnitOfWork.cs
+++ DatabaseLayer/UnitOfWork/UnitOfWork.cs
@@ -1,6 +1,6 @@
-﻿using DatabaseLayer.UnitOfWork;
-using DatabaseLayer.Context;
+﻿using DatabaseLayer.Context;
 using DataBaseLayer.Repositories;
+using DataBaseLayer.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseLayer.UnitOfWork;
@@ -15,19 +15,19 @@ public class UnitOfWork : IUnitOfWork
     public UnitOfWork(AskAHumanDbContext context) => _context = context;
     
     private ChatRepository? _chatRepository;
-    public ChatRepository Chats => _chatRepository ??= new ChatRepository(_context);
+    public IChatRepository Chats => _chatRepository ??= new ChatRepository(_context);
     
     private MessageRepository? _messageRepository;
-    public MessageRepository Messages => _messageRepository ??= new MessageRepository(_context);
+    public IMessageRepository Messages => _messageRepository ??= new MessageRepository(_context);
         
     private PermissionRepository? _permissionRepository;
-    public PermissionRepository Permissions => _permissionRepository ??= new PermissionRepository(_context);
+    public IPermissionRepository Permissions => _permissionRepository ??= new PermissionRepository(_context);
             
     private RoleRepository? _roleRepository;
-    public RoleRepository Roles => _roleRepository ??= new RoleRepository(_context);
+    public IRoleRepository Roles => _roleRepository ??= new RoleRepository(_context);
     
     private UserRepository? _userRepository;
-    public UserRepository Users => _userRepository ??= new UserRepository(_context);
+    public IUserRepository Users => _userRepository ??= new UserRepository(_context);
     
     public void Commit() => _context.SaveChanges();
 
